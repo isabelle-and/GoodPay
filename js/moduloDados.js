3b4bd2737a3dfe87e8d39f1279daf86052b9996a
isabelle-and <isabelle.andreetta@gmail.com>
2022-07-08 10:11:34 -0300

    Adicao sistema de modulos para execucao do Login

---
+++ js/moduloDados.js
@@ -0,0 +1,22 @@
+const userLogin = [
+{
+    id: 1001,
+    userName: "isabelle",
+    senha: "isabelle123",
+    email: "isabelle.andreetta@gmail.com"
+},
+{
+    id: 1002,
+    userName: "marcospinheiro",
+    senha: "m123456",
+    email: "pinheiromarcos@gmail.com"
+},
+{
+    id: 1004,
+userName: "elisamarcondes",
+senha: "123456",
+email: "elisamarcondes@gmail.com"
+},
+];
+
+export default userLogin
